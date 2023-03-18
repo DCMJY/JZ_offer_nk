6a2bdde714a5b8d59d46fb97a69ee2c2a70f4e08
majiayong <490550367@qq.com>
2023-03-18 17:26:04 +0800

    1

--- src/tttt.java
+++ src/tttt.java
@@ -1,5 +1,8 @@
 public class tttt {
     public static void main(String[] args) {
         System.out.println("asdasdasd");
+        System.out.println("asdasdasd");
+        System.out.println("asdasdasd");
+        System.out.println("asdasdasd");
     }
 }

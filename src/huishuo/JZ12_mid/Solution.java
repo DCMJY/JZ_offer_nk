673aec572fde5ec73071795ea14f4ae60d837356
majiayong <490550367@qq.com>
2023-04-03 19:14:37 +0800

    test2

--- src/huishuo/JZ12_mid/Solution.java
+++ src/huishuo/JZ12_mid/Solution.java
@@ -14,7 +14,7 @@ https://leetcode.cn/problems/ju-zhen-zhong-de-lu-jing-lcof/solution/mian-shi-ti-
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 
-package huishuo.JZ12;
+package huishuo.JZ12_mid;
 
 public class Solution {
     public boolean hasPath (char[][] matrix, String word) {
